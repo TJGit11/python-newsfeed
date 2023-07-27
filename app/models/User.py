510d7ac8879852e63b374c41c30d3fc450613217
unknown <thomasjjohnson9311@gmail.com>
2023-07-27 13:19:21 -0500

    use bcrypt to hash password

--- app/models/User.py
+++ app/models/User.py
@@ -1,7 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import validates
+import bcrypt
 
+salt = bcrypt.gensalt()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -18,4 +20,5 @@ class User(Base):
     @validates('password')
     def validate_password(self, key, password):
         assert len(password) > 4
-        return password
+        # encrypt password
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
